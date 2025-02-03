38d800a6ffc239ff8b7981d65ee092416342a0d3
Ang Zhen Yue <boborealmoon@gmail.com>
2025-02-03 21:50:23 +0100

    updates

--- room-booking-app/src/App.js
+++ room-booking-app/src/App.js
@@ -1,20 +1,13 @@
-// src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import BookingPage from './components/BookingPage';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import BookingPage from './components/BookingPage'; // Import BookingPage or your main component
 
 const App = () => {
-  return (
-    <Router>
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<BookingPage />} /> {/* Default route */}
-          {/* Add more routes here as needed */}
-        </Routes>
-      </div>
-    </Router>
-  );
+    return (
+        <div>
+            <BookingPage />
+        </div>
+    );
 };
 
 export default App;
+
